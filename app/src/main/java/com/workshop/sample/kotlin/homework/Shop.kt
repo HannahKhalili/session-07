825a53db8a8906742d14c3b26d351d39381739d1
Hanna Khalili <hana.khalily@gmail.com>
2019-02-24 08:39:26 +0330

    add equal and hash code

--- app/src/main/java/com/workshop/sample/kotlin/homework/Shop.kt
+++ app/src/main/java/com/workshop/sample/kotlin/homework/Shop.kt
@@ -6,6 +6,14 @@ data class Customer(val name: String, val city: City, val orders: List<Order>)
 
 data class Order(val products: List<Product>, val isDelivered: Boolean)
 
-data class Product(val name: String, val price: Double)
+data class Product(val name: String, val price: Double) {
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return hashCode().equals(other.hashCode())
+    }
+}
 
 data class City(val name: String)
